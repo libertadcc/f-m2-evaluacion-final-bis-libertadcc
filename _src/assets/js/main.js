86e7dce63848b847ff701f5f8132227871664cdb
Libertad Chapinal <libertadchapinalcervantes@gmail.com>
2019-05-05 19:47:58 +0200

    First steps

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('>> Ready :)');
+const btn = document.querySelector('.btn');
+const card = document.querySelector('.card');
+const back = document.querySelector('.back');
+
+function show(){
+  fetch ('https://raw.githubusercontent.com/Adalab/cards-data/master/4.json')
+    .then(response=> response.json())
+    .then(data => {
+      for (let i = 0; i<data.length; i++){
+        console.log(data[i].name);
+        back.setAttribute('src', data[i].image);
+      }
+    });
+}
+
+back.addEventListener('click', show);
